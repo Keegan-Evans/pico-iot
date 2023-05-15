2202ab5ea767553e5f62a73de4588ec5510e7d1d
Keegan Evans <keegan.evans@gmail.com>
2023-05-15 14:44:12 -0700

    NEW: AQ sensor can now publish data via mqtt.

--- air_quality.py
+++ air_quality.py
@@ -3,18 +3,21 @@
 from util import try_until_runs, set_timeout
 import utime
 from crc import create_message_packet
+from umqtt.simple import MQTTClient
+
+import json
 
 INIT_AIR_QUALITY = bytearray([0x20, 0x03])
 MEASURE_AIR_QUALITY = bytearray([0x20, 0x08])
 MEASURE_RAW_SIGNALS = bytearray([0x20, 0x50])
 
 class SGP30:
-    def __init__(self, bus, mqtt_connection, address=0x58):
-        utime.sleep_ms(1)
+    def __init__(self, bus, mqtt_broker_addr, mqtt_identity='air_quality_sensor', mqtt_port=1883, address=0x58):
+        utime.sleep_ms(10)
 
         self.address = address
         self.bus = bus
-        self.mqtt = mqtt_connection
+        self.mqtt_handler = MQTTClient(mqtt_identity, mqtt_broker_addr, mqtt_port) #TODO setup authenticated connection
         self.co2 = None
         self.tvoc = None
         self.featureSetVersion = None
@@ -68,26 +71,28 @@ class SGP30:
         # gives sensor time to record measurements, min 20-25ms
         utime.sleep_ms(50)
         read_vals = self.read(6)
-        self._h2 = bytearray([read_vals[0] << 8 | read_vals[1]])
+        self.h2 = bytearray([read_vals[0] << 8 | read_vals[1]])
         self.ethanol = bytearray([read_vals[3] << 8 | read_vals[4]])
         return None
 
     
-    @property
     def measurements(self):
         self.measureRawSignals()
         self.measureAirQuality()
 
-        measurement_values = {'co2': int.from_bytes(self.co2, 'big'),
+        measurement_values = json.dumps({'co2': int.from_bytes(self.co2, 'big'),
                               'tvoc': int.from_bytes(self.tvoc, 'big'),
                               'h2': int.from_bytes(self.h2, 'big'),
                               'ethanol': int.from_bytes(self.ethanol, 'big'),
                               'elapsed_time': self.elapsed_time
-                              }
+                              })
+        print(measurement_values)                      
+        return measurement_values
 
-    def report_measurements(self):
-        pass
-
+    def publish(self):
+        self.mqtt_handler.connect()
+        self.mqtt_handler.publish('air_quality', self.measurements, qos=0)
+        self.mqtt_handler.disconnect()
         
 
 if __name__ == '__main__':
@@ -95,8 +100,8 @@ if __name__ == '__main__':
 
     i2c = setup_I2C_bus()
 
-    test_aq = SGP30(bus=i2c)
+    test_aq = SGP30(bus=i2c, mqtt_broker_addr='192.168.0.5')
 
     while True:
-        print(test_aq.measurements)
-        utime.sleep_ms(250)
+       test_aq.publish()
+       utime.sleep_ms(250)
