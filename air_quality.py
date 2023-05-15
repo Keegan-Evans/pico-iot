b378951c618ebd9078669d89820b1da4306f83e8
Keegan Evans <keegan.evans@gmail.com>
2023-05-15 13:01:51 -0700

    IMP: working on implementing wrapping data up to be shared via mqtt.

--- air_quality.py
+++ air_quality.py
@@ -1,7 +1,7 @@
 # air_quality.py
 
-from _util import try_until_runs, set_timeout
-import time
+from util import try_until_runs, set_timeout
+import utime
 from crc import create_message_packet
 
 INIT_AIR_QUALITY = bytearray([0x20, 0x03])
@@ -9,19 +9,25 @@ MEASURE_AIR_QUALITY = bytearray([0x20, 0x08])
 MEASURE_RAW_SIGNALS = bytearray([0x20, 0x50])
 
 class SGP30:
-    def __init__(self, bus, address=0x58):
-        time.sleep_ms(1)
+    def __init__(self, bus, mqtt_connection, address=0x58):
+        utime.sleep_ms(1)
 
         self.address = address
         self.bus = bus
-        self._co2 = None
-        self._tvoc = None
+        self.mqtt = mqtt_connection
+        self.co2 = None
+        self.tvoc = None
         self.featureSetVersion = None
-        self._h2 = None
-        self._ethanol = None
+        self.h2 = None
+        self.ethanol = None
         self.serialID = None
+        self.start_time = utime.mktime(utime.localtime())
+        self.elapsed_time = 0
 
     
+    def set_elapsed_time(self):
+        self.elapsed_time = utime.mktime(utime.localtime()) - self.start_time
+
     @set_timeout(10)
     @try_until_runs
     def write(self, msg):
@@ -42,50 +48,55 @@ class SGP30:
     def measureAirQuality(self):
         self.write(MEASURE_AIR_QUALITY)
         # gives sensor time to record measurements, min 20-25ms
-        time.sleep_ms(50)
+        utime.sleep_ms(50)
         read_vals = self.read(6)
-        self._co2 = bytearray([read_vals[0] << 8 | read_vals[1]])
-        self._tvoc= bytearray([read_vals[3] << 8 | read_vals[4]])
+        self.set_elapsed_time()
+        
+        # No real values are returned for the co2 or tvoc values until
+        # 15 seconds after the initialization of the sensor so that
+        # proper calibration can occur
+        if self.elapsed_time < 15:
+            self.co2 = bytearray([255 << 8 | 255])
+            self.tvoc = bytearray([255 << 8 | 255])
+        self.co2 = bytearray([read_vals[0] << 8 | read_vals[1]])
+        self.tvoc= bytearray([read_vals[3] << 8 | read_vals[4]])
         return None
 
 
     def measureRawSignals(self):
         self.write(MEASURE_RAW_SIGNALS)
         # gives sensor time to record measurements, min 20-25ms
-        time.sleep_ms(50)
+        utime.sleep_ms(50)
         read_vals = self.read(6)
         self._h2 = bytearray([read_vals[0] << 8 | read_vals[1]])
-        self._ethanol = bytearray([read_vals[3] << 8 | read_vals[4]])
+        self.ethanol = bytearray([read_vals[3] << 8 | read_vals[4]])
         return None
 
     
     @property
-    def CO2(self):
-        return int.from_bytes(self._co2, 'big')
- 
+    def measurements(self):
+        self.measureRawSignals()
+        self.measureAirQuality()
 
-    @property
-    def TVOC(self):
-        return int.from_bytes(self._tvoc, 'big')
- 
+        measurement_values = {'co2': int.from_bytes(self.co2, 'big'),
+                              'tvoc': int.from_bytes(self.tvoc, 'big'),
+                              'h2': int.from_bytes(self.h2, 'big'),
+                              'ethanol': int.from_bytes(self.ethanol, 'big'),
+                              'elapsed_time': self.elapsed_time
+                              }
 
-    @property
-    def H2(self):
-        return int.from_bytes(self._h2, 'big')
- 
+    def report_measurements(self):
+        pass
 
-    @property
-    def ethanol(self):
-        return int.from_bytes(self._ethanol, 'big')
+        
 
 if __name__ == '__main__':
-    from _util import setup_I2C_bus
+    from util import setup_I2C_bus
 
     i2c = setup_I2C_bus()
 
     test_aq = SGP30(bus=i2c)
-    test_aq.start_measuring_aq()
 
     while True:
-        print("CO2: {}\nVOC: {}\nH2: {}\nEthanol: {}\n\n".format(test_aq.CO2, test_aq.TVOC, test_aq.H2, test_aq.ethanol))
-        time.sleep(5)
+        print(test_aq.measurements)
+        utime.sleep_ms(250)
