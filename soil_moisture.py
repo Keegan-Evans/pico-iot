0e10dbe73a46c8fc8f96556c6152ca6783452521
Keegan Evans <keegan.evans@gmail.com>
2023-06-06 10:39:46 -0700

    Changed mqtt_handle to optional for soil moisture sensor

--- soil_moisture.py
+++ soil_moisture.py
@@ -10,7 +10,7 @@ print(adc.read_u16())
 #print(adc.read_uv())
 
 class Moisture_Sensor:
-    def __init__(self, mqtt_handle, pin_num=28, sensor_id="soil_moisture_1", topic="sensor/moisture", indicator_pin="LED"):
+    def __init__(self, mqtt_handle=None, pin_num=28, sensor_id="soil_moisture_1", topic="sensor/moisture", indicator_pin="LED"):
        time.sleep_us(100)
 
        self.sensor_id = sensor_id
@@ -21,17 +21,20 @@ class Moisture_Sensor:
        self.calibration_value_high = 65535
        self.indicator_pin = Pin(indicator_pin, Pin.OUT)
        self.mqtt_handle = mqtt_handle
-       
+
     def toggle_indicator(self):
         self.indicator_pin.toggle()
-       
+
     def read_moisture(self):
         self.indicator_pin.on()
         self._raw_value = self.pin.read_u16()
         time.sleep(1)
         self.indicator_pin.off()
         print(self._raw_value)
-        return None
+        if self.mqtt_handle is None:
+            return self._raw_value
+        else:
+            return None
         # return self._raw_value.to_bytes(2, 'big')
 
     def publish(self):
@@ -42,8 +45,8 @@ class Moisture_Sensor:
         self.mqtt_handle.disconnect()
 
 
- 
-    
+
+
     # def set_low_calibration(self):
     #     self.indicator_pin.on()
     #     calibration_reads = []
@@ -53,14 +56,14 @@ class Moisture_Sensor:
     #         read = self.raw_read
     #         print(read)
     #         calibration_reads.append(read)
-            
+
     #     calibration_reads.remove(max(calibration_reads))
     #     calibration_reads.remove(min(calibration_reads))
-            
+
     #     self.calibration_value_low = sum(calibration_reads)/len(calibration_reads)
     #     self.indicator_pin.off()
-        
-        
+
+
     # def set_high_calibration(self):
     #     self.indicator_pin.on()
     #     calibration_reads = []
@@ -70,21 +73,21 @@ class Moisture_Sensor:
     #         calibration_reads.append(self.raw_read)
     #         #Pin(25).on()
     #         print(calibration_reads)
-            
+
     #     calibration_reads.remove(max(calibration_reads))
     #     calibration_reads.remove(min(calibration_reads))
-            
+
     #     self.calibration_value_high = sum(calibration_reads)/len(calibration_reads)
     #     print(self.calibration_value_high)
     #     self.indicator_pin.off()
-        
-    
-   
+
+
+
 #    def calibrated_read(self):
 #        p1 = (0, self.calibration_value_low)
 #        p2 = (100, self.calibration_value_high)
 #        m = (p2[1] - p1[1])/(p2[0] - p1[0])
-       
+
 #        x = ((self.raw_read - p1[1])/m)
 #        return x
 
@@ -98,7 +101,7 @@ class Moisture_Sensor:
 #     print(list(rel_vals))
 #     #rel_vals = lambda x: abs(input_map - raw_val)
 #     rel_val = min(input_map, key=rel_vals)
-#     return output_map[rel_val] 
+#     return output_map[rel_val]
 
 # def closest_value(input_map, input_value):
 #     mapped = [abs(x - input_value) for x in input_map]
@@ -107,7 +110,7 @@ class Moisture_Sensor:
 #     #i = mapped.argmin()
 #     #print(i)
 #     #print(arr[i])
-            
+
 if __name__ == '__main__':
     from test_setup import setup_for_testing
     from util import setup_I2C_bus
