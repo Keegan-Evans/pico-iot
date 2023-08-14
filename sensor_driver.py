306aff6f09c8eedb0c68542d6afbd61fbacc7a05
Keegan Evans <keegan.evans@gmail.com>
2023-08-14 16:26:08 -0700

    FEAT: working on generating measurement method for each target sensor method

--- sensor_driver.py
+++ sensor_driver.py
@@ -3,20 +3,39 @@
 class SensorDriver:
     
     # populate init method parameters from current attributes and add default values
-
+    # TODO: refactor sensor measurements to be a dictionary of pin: measurement
     def __init__(self, i2c_bus=None, i2c_address=None, network_connection=None,
                  mqtt_client=None, sensor_pins={}, topic="sensor_data/",
-                 sensor_id="sensor", indicator_pin="LED", i2c_command_dict={}, sensor_measurements=[]) -> None:
+                 sensor_id="sensor", indicator_pin="LED", i2c_command_dict={}, sensor_measurements=None) -> None:
         # create class attributes for ic2 bus and address, network object, mqtt client, sensor pins, sensor outputs, topic, sensor id, and start time, indicator pin, i2c command dictionary, and sensor variables
-        self.i2c_bus = None
-        self.i2c_address = None
-        self.network_connection = None
-        self.mqtt_client = None
-        self.sensor_pins = {} # pin: measurement
-        self.topic = None
-        self.sensor_id = None
-        self.start_time = None
-        self.indicator_pin = None
-        self.i2c_command_dict = None
-        self.sensor_measurements = []
-        
+        self.i2c_bus = i2c_bus
+        self.i2c_address = i2c_address
+        self.network_connection = network_connection
+        self.mqtt_client = mqtt_client
+        self.sensor_pins = sensor_pins# pin: measurement
+        self.topic = topic
+        self.sensor_id = sensor_id
+        self.indicator_pin = indicator_pin
+        self.i2c_command_dict = i2c_command_dict
+        self.sensor_measurements = sensor_measurements
+
+        if self.sensor_measurements is not None:
+            for each in self.sensor_measurements:
+                setattr(self, each, None)
+                private_func_name = "_measure_{}".format(each)
+                attr_name = "measure_{}".format(each)
+                self.attr_name = lambda : self._measure()
+
+    # create method to collect each sensor measurement from self.sensor_measurements and add an attribute to the class for each
+    # def collect_sensor_measurements(self):
+        # for each in self.sensor_measurements:
+            # set attribute for each sensor measurement
+            # setattr(self, each, None)
+# 
+            # self.__dir__().append("measure_{}".format(each))
+
+    def bogus_method(self):
+        pass
+
+            
+            
--- tests/test_sensor_driver.py
+++ tests/test_sensor_driver.py
@@ -1,4 +1,5 @@
 from sensor_driver import SensorDriver
+#import inspect
 
 def test_instantiation():
     sensor_driver = SensorDriver()
@@ -7,7 +8,7 @@ def test_instantiation():
 def test_default_class_attributes():
     sensor_driver = SensorDriver()
 
-    expected_attributes = ['i2c_bus', 'i2c_address', 'network_connection', 'mqtt_client', 'sensor_pins', 'topic', 'sensor_id', 'start_time', 'indicator_pin',
+    expected_attributes = ['i2c_bus', 'i2c_address', 'network_connection', 'mqtt_client', 'sensor_pins', 'topic', 'sensor_id', 'indicator_pin',
     'i2c_command_dict', 'sensor_measurements']
 
     for attribute in expected_attributes:
@@ -17,6 +18,13 @@ def test_default_class_attributes():
             print(f"Attribute {attribute} not found in SensorDriver class")
             raise AssertionError
 
+def test_create_measurement_methods():
+    test_driver = SensorDriver(sensor_measurements = ['temperature', 'humidity', 'pressure', 'altitude'])
+    print(test_driver.__dict__)
+    #print(inspect.getmembers(test_driver))
+
 if __name__ == '__main__':
     test_instantiation()
     test_default_class_attributes()
+    test_create_measurement_methods()
+
